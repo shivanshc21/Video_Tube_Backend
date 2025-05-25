e963cf1bed435f3fbe8196fbf1014b2efe8f504b
Shivansh Chaurasiya <shivanshchaurasiya2004@gmail.com>
2025-05-25 14:17:08 +0530

    corrected bugs

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,6 @@
 import  { Router } from "express";
 import  { registerUser } from "../controllers/user.controller.js";
-import {upload} from "../utils/multer.js";   // multer is a middleware in uploading files to cloudinary
+import {upload} from "../middlewares/multer.middleware.js";   // multer is a middleware in uploading files to cloudinary
 
 const router = Router();
 
--- src/utils/apiError.js
+++ src/utils/apiError.js
@@ -1,4 +1,4 @@
-class ApiError extends Errors {
+class ApiError extends Error {
     constructor(
         statusCode,
         message = "Something went wrong",
