282ced4c49e2a5657c080cfda8e0408d2ce377f0
Shivansh Chaurasiya <shivanshchaurasiya2004@gmail.com>
2025-05-31 13:59:31 +0530

    Add Like, Playlist, and Tweet models with schema definitions

--- src/models/comment.model.js
+++ src/models/comment.model.js
@@ -1,4 +1,4 @@
-import mongoose,{Schema} from "mongoose";
+import mongoose, {Schema} from "mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const commentSchema = new Schema(
@@ -22,3 +22,5 @@ const commentSchema = new Schema(
 )
 
 commentSchema.plugin(mongooseAggregatePaginate);
+
+export const Comment = mongoose.model("Comment",commentSchema)
