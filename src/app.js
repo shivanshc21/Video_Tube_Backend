7473785ccec414ea35134b1f259fb44d1fc88924
Shivansh Chaurasiya <shivanshchaurasiya2004@gmail.com>
2025-06-06 13:19:06 +0530

    Add like management functionality: implement like routes and controller methods for toggling likes on videos, comments, and tweets; update like model to support multiple entities

--- src/app.js
+++ src/app.js
@@ -25,6 +25,7 @@ import subscriptionRouter from "./routes/subscription.routes.js"
 import commentRouter from "./routes/comment.routes.js"
 import tweetRouter from "./routes/tweet.routes.js"
 import playlistRouter from "./routes/playlist.routes.js"
+import likeRouter from "./routes/like.routes.js"
 
 //Routes Declaration (middlewares used)
 app.use("/api/v1/users", userRouter);      // https://localhost:8000/api/v1/users/register
@@ -33,5 +34,6 @@ app.use("/api/v1/subscriptions",subscriptionRouter)
 app.use("/api/v1/comments",commentRouter)
 app.use("/api/v1/tweets",tweetRouter)
 app.use("/api/v1/playlists",playlistRouter)
+app.use("/api/v1/likes",likeRouter)
 
 export { app };
